123c62cfe64a2afd5636c12bdd623652e2e7401b
chanhong-dev <zxck03051@gmail.com>
2021-11-15 22:51:22 +0900

    2021-11-15

--- java/Java4/Java4_5.java
+++ java/Java4/Java4_5.java
@@ -1,11 +1,27 @@
 package Java4;
 
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Java4_5 {
     public int solution(int N, int K, int[] num){
-
-        return 0;
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int l = j + 1; l < N; l++) {
+                    Tset.add(num[i] + num[j] + num[l]);
+                }
+            }
+        }
+        int cnt = 0;
+        for (int x :
+                Tset) {
+            cnt++;
+            if(cnt == K)
+                return x;
+        }
+        return -1;
     }
     public static void main(String[] args) {
         Java4_5 java4_5 = new Java4_5();
