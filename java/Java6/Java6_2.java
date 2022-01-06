1a4419513a5eaf142de1c5e73e3b59141c2afae3
chanhong-dev <zxck03051@gmail.com>
2022-01-06 20:54:55 +0900

    2022-01-06

--- java/Java6/Java6_2.java
+++ java/Java6/Java6_2.java
@@ -1,4 +1,29 @@
 package Java6;
 
+import java.util.*;
+
 public class Java6_2 {
+    public int[] solution(int N, int[] arr) {
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Java6_2 java6_2 = new Java6_2();
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scan.nextInt();
+        }
+        for (int x : java6_2.solution(N, arr)) System.out.print(x + " ");
+    }
 }
